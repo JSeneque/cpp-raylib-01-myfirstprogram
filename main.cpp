e5bb95d767791e9e57ec6ef1d9878b64484c4761
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2021-10-25 20:40:44 +1030

    initial version

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(800, 600, "My First Program");
+
+    Vector2 ballPosition = { 400.0f,300.0f };
+;    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        
+        // clears the screen
+        ClearBackground(BLACK);
+
+        // get the mouse position
+        Vector2 mousePosition = GetMousePosition();
+
+        ballPosition = mousePosition;
+
+        DrawCircleV(ballPosition, 20.0f, BLUE);
+
+        EndDrawing();
+    }
+    return 0;
+}
